37f49a546bde944f474d9e6679f0d7d0fdfce997
Michael Louiset <72944133+Universe-art@users.noreply.github.com>
2022-01-17 10:51:36 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,110 @@
+
+import React from 'react'
+import Header from "./components/Header"
+import Tasks from "./components/Tasks"
+import AddTask from "./components/AddTask"
+import {useState, useEffect} from 'react';
+
+
+
+const  App= () =>{
+  const [showAddTask, setShowAddTask] = useState(true);
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+      const getTasks = async () => {
+        const tasksFromServer =  await fetchTasks()
+        setTasks(tasksFromServer)
+      }
+    getTasks()
+  }, [])
+
+  //Fetch Tasks 
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+    return data
+  }
+
+
+    //Fetch Tasks 
+    const fetchTask = async (id) => {
+      const res = await fetch(`http://localhost:5000/tasks/${id}`)
+      const data = await res.json()
+      return data
+    }
+
+
+  //AddTask
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(task),
+    })
+
+    const data = await res.json()
+    console.log(data)
+    setTasks([...tasks, data])
+
+    // const id = Math.floor(Math.random() * 10000);
+    // const newTask = {id, ...task}
+    // setTasks([...tasks, newTask])
+  } 
+
+  
+
+  //Delete task
+  const deleteTask = async(id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'DELETE',
+    })
+
+    console.log('delete ', id)
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  // Toggle Reminder
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id)
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(updTask),
+    })
+
+    const data = await res.json()
+
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      )
+    )
+  }
+  
+
+  return (
+    <div className="container">
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      {showAddTask && <AddTask onAdd={addTask}/>}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'Pas de task' }
+
+    
+    </div>
+
+    //Expression question ? alors : sinon
+    //is this condition true ? yes : no
+    //{tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}/> : 'Pas de task' }
+  
+  );
+}
+
+
+
+export default App;
